a96fba14e2a1186fd4547386ef22d8878a74d9cd
Roberto Alencar <rroberto.alencar@gmai.com>
2022-11-08 11:26:42 -0300

    enviando arquivos iniciais para o projeto

---
+++ src/main/java/br/com/ifpe/clienteconsumer/ClienteConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.clienteconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/clienteconsumer/ClienteConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.clienteconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
